08e458934fe2bd1a2c2b3b81f83454b7553bedbd
henteko <henteko07@gmail.com>
2017-01-24 15:56:47 +0900

    init

---
+++ CarthageSampleLibrary/CarthageSampleLibrary.h
@@ -0,0 +1,19 @@
+//
+//  CarthageSampleLibrary.h
+//  CarthageSampleLibrary
+//
+//  Created by kenta.imai on 2017/01/24.
+//  Copyright © 2017年 henteko07. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageSampleLibrary.
+FOUNDATION_EXPORT double CarthageSampleLibraryVersionNumber;
+
+//! Project version string for CarthageSampleLibrary.
+FOUNDATION_EXPORT const unsigned char CarthageSampleLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageSampleLibrary/PublicHeader.h>
+
+
---
+++ CarthageSampleLibrary/CarthageSampleLibrary.swift
@@ -0,0 +1,15 @@
+//
+//  CarthageSampleLibrary.swift
+//  CarthageSampleLibrary
+//
+//  Created by kenta.imai on 2017/01/24.
+//  Copyright © 2017年 henteko07. All rights reserved.
+//
+
+import UIKit
+
+public class CarthageSampleLibrary: NSObject {
+    public func hello(message: String) {
+        print("CarthageSampleLibrary: \(message)")
+    }
+}
